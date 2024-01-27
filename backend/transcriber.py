8f65e4c0338358aaeeafd017400e3db607b0214b
AndrewHC36 <andrewshen31@gmail.com>
2024-01-27 12:03:02 -0800

    working basic transcriber

--- backend/transcriber.py
+++ backend/transcriber.py
@@ -1,25 +1,38 @@
 import requests
 from google.cloud import speech_v1
+import base64
 
 
 def transcriber() -> str:
+    with open("./audio/sample.wav", "rb") as fobj:
+        fdata = fobj.read()
+
+    audio_blob = base64.b64encode(fdata)
+    # print(audio_blob)
+
     client = speech_v1.SpeechClient()
 
     config = speech_v1.RecognitionConfig()
-    config.encoding = "FLAC"
-    config.sample_rate_hertz = 16000
+    config.audio_channel_count = 1
+    config.encoding = "LINEAR16"
+    config.sample_rate_hertz = 48000
     config.language_code = "en-US"
     config.enable_word_time_offsets = False
+    # config.model = "latest_short"
+    # config.enable_word_confidence = True
+    # config.metadata.recording_device_type = "SMARTPHONE"
 
     audio = speech_v1.RecognitionAudio()
-    # audio.content = b'content_blob'
-    audio.uri = "gs://cloud-samples-tests/speech/brooklyn.flac"
+    audio.content = audio_blob.decode("ascii")
+    # audio.uri = "gs://cloud-samples-tests/speech/brooklyn.flac"
 
     request = speech_v1.RecognizeRequest(config = config, audio = audio)
 
     response = client.recognize(request=request)
 
-    print(response)
+    print("Billed Time:", response.total_billed_time)
+    print("Results:", response.results)
+    print("Adaptations:", response.speech_adaptation_info)
 
 
     # resp = requests.post(
