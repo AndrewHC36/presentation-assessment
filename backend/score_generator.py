9058aac12cea95d29458009cd9f2bb4961bdd6a3
Rueih-YouFu <fu22@ucsb.edu>
2024-01-28 03:00:18 -0800

    basic score generator

--- backend/score_generator.py
+++ backend/score_generator.py
@@ -1,18 +1,33 @@
 from analyzer import AnalysisResult
+from analyzer_prowritingaid import filler, analyze
 
-
-def score_generator(analysis: AnalysisResult) -> float:
+def score_generator(analysis: str) -> float:
     score = 100
-    for err in analysis.errors:
-        print(err)
-        size = err.error_len / 5  # characters per error
+    total = 0
+    weights = [2, 2, 2, 2, 3]
 
-        # https://www.w3.org/International/multilingualweb/lt/drafts/its20/its20.html#lqissue-typevalues
-        match err.issue_type:
-            case "misspelling":
-                factor = 5.0
 
-        score -= size*factor
+    i = analyze(analysis)
+    for num in range(len(weights)):
+        total += weights[num] * i[num]
+    return (100 - (total + filler(analysis)))
 
-    return score
+
+print(score_generator("Along the way, he must face like a host of mythological like enemies determined to stop him. Most like of all, he must come to terms um with a uh father he has never known, and an Oracle that has warned him of betrayal by a friend."))
+
+
+# def score_generator1(analysis: AnalysisResult) -> float:
+#     score = 100
+#     for err in analysis.errors:
+#         print(err)
+#         size = err.error_len / 5  # characters per error
+
+#         # https://www.w3.org/International/multilingualweb/lt/drafts/its20/its20.html#lqissue-typevalues
+#         match err.issue_type:
+#             case "misspelling":
+#                 factor = 5.0
+
+#         score -= size*factor
+
+#     return score
 
